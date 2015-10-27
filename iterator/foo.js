b06982935a3c4ce0b0b7ef7755ec648076ae5c9a
SamHwang1990 <samhwang1990@gmail.com>
2015-10-27 13:52:45 +0800

    for of demo

--- iterator/foo.js
+++ iterator/foo.js
@@ -9,8 +9,23 @@
   // console.log(tempArray.toString());
 
   for (var j of tempArray) {
-    if (j % 5 > 0) return;
+    if (j % 5 > 0) continue;
     console.log(j);
+  }
+
+  let tempFactory = function(name) {
+    this.name = name;
+  };
+  tempFactory.prototype.parentName = 'djj';
+
+  let tempObj = new tempFactory('xjj');
+
+  for (let z of Object.keys(tempObj)) {
+    console.log(z, tempObj[z]);
+  }
+
+  for (let z in tempObj) {
+    console.log(z, tempObj[z]);
   }
 
 })();
