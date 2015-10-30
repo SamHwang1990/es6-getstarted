2cc1e30f5ccc221b1c0160360035b07a308b0273
SamHwang1990 <samhwang1990@gmail.com>
2015-10-30 20:44:26 +0800

    writing promise demo

--- promise/foo.js
+++ promise/foo.js
@@ -2,6 +2,30 @@
  * Created by sam on 15/10/30.
  */
 
-+(function() {
-  'use strict';
-})();
+'use strict';
+
+var fs = require('fs'),
+    path = require('path');
+
+var getJSON = function(name) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(path.resolve('./db', name), function(err, data) {   // relative to foo.js
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(data);
+
+    });
+  }).then(JSON.parse);
+};
+
+var output = function(name, content, callback) {
+  fs.writeFile(path.resolve('./', 'output.txt'), content + '\r\n', callback);
+};
+
+getJSON('story.json').then(function(story) {
+  fs.writeFile(path.resolve('./', 'output.txt'), data.heading, function(err) {
+    if (err) throw err;
+  });
+});
