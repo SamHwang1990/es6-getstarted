34b8552e48686fa02060bebb535501f3eb6fb54d
SamHwang1990 <samhwang1990@gmail.com>
2016-06-14 23:23:10 +0800

    add Map and WeakMap practices

--- setMap/Map.js
+++ setMap/Map.js
@@ -1,3 +1,64 @@
 /**
  * Created by Administrator on 2016/6/14.
  */
+
+;(function() {
+
+  // Map 提供一种比Object 更完善的hash 实现
+  // Object 也是一种hash 实现,但规定,键的类型只能是字符串或Symbol
+  // Map 的键可以是任意类型
+  // 既然是任意类型的键,就有必要关心如何判断同名属性
+  // Map 对键值使用的重复算法于Set 一样:
+  //    非引用类型直接判断值
+  //    引用类型就要判断起内存地址是否一致
+
+  // 构造函数允许以数组形式传入键值对来初始化
+
+  // 实例属性:
+  //    size: Map 中的键值对总数
+
+  // 实例方法:
+  //    set(key, value): 设置键值对,如果出现同名键,新设置的值会覆盖旧的值,方法返回Map 实例,方便链式写法
+  //    get(key): 读取key 对应的值,找不到则返回undefined
+  //    has(key): 返回Map 实例中是否包含key
+  //    delete(key): 删除Map 实例中key 对应的整个键值对
+  //    clear(): 清空Map 中的数据
+  //    keys(): 返回Map 中键名的Iterator 遍历器
+  //    values(): 返回Map 中值的Iterator 遍历器
+  //    entries(): 返回Map 中键值对的Iterator 遍历器，键值对item 是一个数组（[key, value]）
+  //    forEach(): 遍历Map
+
+  {
+
+    let map = new Map([['name', 'sam'], [{age: 18}, (age) => console.log(age)]]);
+
+    console.log(map.get('name'));
+    console.log(map.has('name'));
+    console.log(map.has({age: 18}));
+
+    let age = {age: 18};
+    console.log(map.set(age, (age) => console.log(age * 2)).has(age));
+
+    console.log(map.size);
+
+    for (let key of map.keys()) {
+      console.log(key);
+    }
+
+    for (let value of map.values()) {
+      console.log(value);
+    }
+
+    for (let [key, value] of map.entries()) {
+      console.log(key, value);
+    }
+
+    // 默认for...of 遍历的是entries
+    console.log(map[Symbol.iterator] === map.entries);
+    for (let entry of map) {
+      console.log(entry[0], entry[1]);
+    }
+
+  }
+
+})();
--- setMap/WeakMap.js
+++ setMap/WeakMap.js
@@ -1,3 +1,56 @@
 /**
  * Created by Administrator on 2016/6/14.
+ *
+ * 参考:
+ *    1. http://stackoverflow.com/questions/30556078/ecmascript-6-what-is-weakset-for
+ *    2. http://es6.ruanyifeng.com/#docs/set-map#WeakMap
  */
+
+;(function() {
+
+  // WeakMap 与Map 有着非常不一样的用途和用法
+  // 相同点：
+  //    1. 以键值对方式阻止数据
+
+  // API 不同点：
+  //    1. 不提供遍历接口,比如:keys(), values(), entries(), forEach()
+  //    2. 不提供clear()
+  //    3. 键的类型只能是对象类型
+  //    4. 对键对象是弱引用,即Map 不会持有该对象的引用
+
+  // 用途 不同点：
+  //    1. 键是弱引用，即WeakMap 实例不会持有其值，意味着，GC 在判断对象是否应该清除时，不会将WeakMap 对其的引用计算在内
+  //    2. 弱引用导致的结果是，如果该键对象的引用次数降为0，则会自动从WeakMap 中自动清除,对应的value 也会被清除
+  //    3. 从开放的接口来看，WeakMap 的用途当然还是键值对关系的存储
+  //       该对应关系是,只要键对象还在内存中,没被GC 清除,那么对应的值就会伴随存在
+  //       只要键对象被GC 清除,对应的值也会被自动清除,不用担心键对象以及值的内存泄漏
+
+  // 下面使用一个WeakMap 来实现私有属性
+  {
+    let _counter = new WeakMap();
+    let _action = new WeakMap();
+
+    class Countdown {
+      constructor(count, action) {
+        _counter.set(this, count);
+        _action.set(this, action);
+      }
+      count() {
+        let counter = _counter.get(this);
+
+        if (counter <= 1) {
+          _action.get(this)();
+          return;
+        }
+
+        _counter.set(this, --counter);
+      }
+    }
+
+    let c3 = new Countdown(3, () => console.log('the end'));
+    c3.count();
+    c3.count();
+    c3.count();
+  }
+
+})();
