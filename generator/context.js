59a0851e669ffbf28d03da77a940642aaf506ff8
SamHwang1990 <samhwang1990@gmail.com>
2016-06-16 08:36:10 +0800

    do not use this object in generator

--- generator/context.js
+++ generator/context.js
@@ -4,10 +4,11 @@
 
 ;(function() {
 
-  // 这里单独说些Generator 函数内部上下文, 即this, 的问题
+  // 这里单独说下Generator 函数内部上下文的问题,即this
   // 一句话说明: Generator 函数不能被用来当作构造函数调用,会抛异常
   // 导致的结果是,在Generator 内部使用的this 是不会返回给外界的,
   // 所以尝试给this 添加属性方法啊等操作都是无意义的,因为外界根本拿不到
+  // 而且Generator 内部如果给this 添加属性,非严格模式下会把属性、方法写到全局作用域中,会造成污染,要避免。
 
   // 不过,Generator 调用却是能返回一个生成器对象,该对象能拿到Generator.prototype 上的属性
 
@@ -27,6 +28,7 @@
     }
 
     console.log([...Count(5)]);   // 这次解构表明,虽然外界拿不到this, 但是在函数内部还是存在这么一个对象可以拿来用的
+    console.log(global.max);      // 但其实,Generator 内部的this 一般指向了window 或global,相当于该了全局属性,一定要避免
   }
 
   {
