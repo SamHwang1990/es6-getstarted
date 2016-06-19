7b35d907408ac639897f36f8f9e9d6192e9912bb
SamHwang1990 <samhwang1990@gmail.com>
2016-06-19 22:56:46 +0800

    add notes

--- class/extends.js
+++ class/extends.js
@@ -4,6 +4,27 @@
 
 ;(() => {
 
+  // class 类定义使用extends 来实现继承
+  // 原理就是原型链扩展
+  // 继承机制涉及四个对象、两个属性、一个方法:
+  // 对象:Child、Child.prototype、Parent、Parent.prototype
+  // 属性:Child.__proto__、Child.prototype.__proto__
+  // 方法:Parent.isPrototypeOf()、Parent.prototype.isPrototypeOf()
+
+  // 声明一点,上面提到的Child、Parent 指的是类的定义,本质上是个函数对象
+
+  // 继承机制是:
+  // 1. 将Parent 设为Child 的原型,作用是静态方法的继承
+  // 2. 将Parent.prototype 设为Child.prototype 的原型,作用是实例方法的继承、构造函数的继承
+  // 3. 提供super 关键字来调用原型对象的方法或构造函数:
+  //    super(...args) 为调用原型对象的构造函数
+  //    super.props(...args) 为调用原型对象的props 方法
+
+  // 使用class 类定义提供的继承机制,要注意以下两点:
+  // 1. 子类的构造函数(constructor) 中,如果要使用"this"对象,就必须先调用super(...args), 交由父类的构造函数来创建"this" 对象
+  // 2. 子类构造函数如果不使用"this", 并返回一个新的对象,则可以不调用super(...args)
+
+
   {
     let Animal = class {
       constructor(type) {
@@ -46,7 +67,7 @@
       getType() {
         return super.type.toUpperCase();
       }
-    }
+    };
 
     let singleDog = new Dog('single', 26);
     console.log(singleDog.type);
@@ -58,13 +79,16 @@
     console.log(Animal.isPrototypeOf(Dog));
   }
 
+  // ES6 函数内部新增"new.target" 属性来判断当前调用是否是以new 关键字来调用
+  // 如果new.target 等于undefind, 表示普通函数调用
+  // 如果new.target 不等于undefined, 表示构造函数形式调用
   {
     let Dog = function() {
       if (new.target == undefined) {
         throw new Error('Dog is a constructor')
       }
       this._type = 'dog';
-    }
+    };
     let singleDog = Dog();
   }
 
